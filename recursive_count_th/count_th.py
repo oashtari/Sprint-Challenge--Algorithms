82a533a6881677f4a3d747a68dddebbcb1c5f6ae
Omid Ashtari <oashtari@gmail.com>
2020-10-11 18:23:37 -0700

    finished counting all the damn th's

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,4 +7,47 @@ def count_th(word):
     
     # TBC
     
-    pass
+    sub = 'th'
+    count = 0
+
+    word_len = len(word)
+    sub_len = len(sub)
+
+    if (word_len == 0 or word_len < sub_len):
+        return 0
+
+    if (word[ : sub_len] == sub):
+        # count +=1
+        # print('elif', word)
+        # print(count)
+        return count_th(word[sub_len-1:]) + 1
+    
+    else:    
+        # print('else', word)
+        # print(count)
+        return count_th(word[sub_len-1:])
+
+    # print('count', count)
+
+    return count 
+
+    # non-functional code that I don't want to throw away
+    # in case I come back to work on this later
+    
+    
+    # if word[0] == 't' and word[1] == 'h':
+    #     count +=1
+
+    # count += count_th(word[1:])
+
+    # return count 
+
+
+
+if __name__ == '__main__':
+
+    word = 'hitherethouthat'
+    print(count_th(word))
+
+    word2 = 'thhtthht'
+    print(count_th(word2))
